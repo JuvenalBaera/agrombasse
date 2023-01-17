def86361d0b0add7f584a7afe403ed195edd8d3d
Juvenal Baera <92170141+JuvenalBaera@users.noreply.github.com>
2023-01-17 20:42:27 +0000

    ...

--- script/main.js
+++ script/main.js
@@ -1,20 +1,23 @@
 let btns = [...document.getElementsByClassName("botoes")]
-let ifr = [...document.getElementsByTagName("iframe")][0]
+let ifrs = [...document.getElementsByTagName("iframe")]
 
 const plantacao = "Plantação👇"
 const equipe = "Equipe👇"
 
 btns[0].addEventListener("click", ()=>{
     btns[1].innerHTML = equipe
-    troca(btns[0], plantacao)
+    troca(ifrs[1], btns[0], plantacao)
 })
 btns[1].addEventListener("click", ()=>{
     btns[0].innerHTML = plantacao
-    troca(btns[1], equipe)
+    troca(ifrs[1], btns[1], equipe)
 })
 
+document.getElementById("planoS").addEventListener("click", ()=>{
+    troca(ifrs[0], document.getElementById("planoS"), "aqui")
+})
 
-function troca(btn, newName){
+function troca(ifr, btn, newName){
     if(ifr.style.display == "none"){
         ifr.style.display = "block"
         btn.innerHTML = "Fechar	❌"
