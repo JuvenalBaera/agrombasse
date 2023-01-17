dab5beffa4d3cfbbeb36adfdd85ed422fca23960
Juvenal Baera <92170141+JuvenalBaera@users.noreply.github.com>
2023-01-17 17:13:12 +0000

    okkayy

--- script/main.js
+++ script/main.js
@@ -2,15 +2,15 @@ let btns = [...document.getElementsByClassName("botoes")]
 let ifr = [...document.getElementsByTagName("iframe")][0]
 
 const plantacao = "Plantação👇"
-const trabalhador = "Trabalhadores👇"
+const equipe = "Equipe👇"
 
 btns[0].addEventListener("click", ()=>{
-    btns[1].innerHTML = trabalhador
+    btns[1].innerHTML = equipe
     troca(btns[0], plantacao)
 })
 btns[1].addEventListener("click", ()=>{
     btns[0].innerHTML = plantacao
-    troca(btns[1], trabalhador)
+    troca(btns[1], equipe)
 })
 
 
